5443bc1bc3cd50ff7ede9cf893cb68b1e800d01f
DrZsombi123 <118626431+DrZsombi123@users.noreply.github.com>
2024-10-13 11:00:02 +0200

    hf

--- 4_1_2.py
+++ 4_1_2.py
@@ -1,5 +1,8 @@
 szam = int(input("Adj meg egy szamot."))
-if szam % 2 == 0:
+
+if szam == 0:
+    print("A szám nem páros és nem is páratlan")
+elif szam % 2 == 0:
     print("A szám páros.")
 
 else:

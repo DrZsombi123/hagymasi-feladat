ee8dc75bbff41d65773611977d582b1966794ee4
DrZsombi123 <118626431+DrZsombi123@users.noreply.github.com>
2024-10-13 10:43:10 +0200

    4_1_2.py

--- 4_1_2.py
+++ 4_1_2.py
@@ -1,6 +1,6 @@
 szam = int(input("Adj meg egy szamot."))
 if szam % 2 == 0:
-    print("A szám osztható kettővel.")
+    print("A szám páros.")
 
 else:
-    print("As szam nem osztható kettővel.")
+    print("As szam páratlan.")
